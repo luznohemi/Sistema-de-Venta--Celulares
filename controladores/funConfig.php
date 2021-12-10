d328d803ddd2b5115f2b7c75b0b3c7b4e7358065
luznohemi <lalvaro.191ci01@istta.edu.pe>
2021-12-10 05:45:32 -0500

    UPDATE

--- controladores/funConfig.php
+++ controladores/funConfig.php
@@ -1,16 +1,22 @@
 <?php
+include_once("conexion.php");
+
 function verConfig(){
     global $pdo;
     $config=[];
-    $sql= "SELECT * FROM Configuracion";
+    $sql= "SELECT*FROM configuracion";   
     $resultado=$pdo->query($sql);
     $resultado->setFetchMode(PDO::FETCH_ASSOC);
-    while ($row=$resultado->fetch()) {
-        array_push($config,$row);
-    }
-    return($config);
 
+        while ($row=$resultado->fetch()) {
+            array_push($config,$row);
+            return $config;
+        }
+   
 }
+verConfig();
+$data=$config;
+ 
 
 function EditarConfig($id,$nombre,$celular,$email,$direccion){
     global $pdo;
